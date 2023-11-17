74c9352b74b3c2a69e8d2c479c558f8c4d75612b
FAHD <mahfoudifahd55@gmail.com>
2023-11-17 09:15:09 +0100

    edit project structure

--- src/main/java/com/rentalhive/repositories/DemandeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rentalhive.repositories;
-
-import com.rentalhive.models.entities.Demand;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DemandeRepository extends JpaRepository<Demand , Long> {
-}
