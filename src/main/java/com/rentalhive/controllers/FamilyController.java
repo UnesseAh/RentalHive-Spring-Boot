b35d0af8d3fe9aef7e27ddf32b48ced444cd4d0f
UnesseAh <youness.ahasla@gmail.com>
2023-11-17 17:36:09 +0100

    create crud for FamilyController

--- src/main/java/com/rentalhive/controllers/FamilyController.java
+++ src/main/java/com/rentalhive/controllers/FamilyController.java
@@ -1,11 +1,13 @@
 package com.rentalhive.controllers;
 
+import com.rentalhive.models.entities.Family;
 import com.rentalhive.services.family.FamilyService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/families")
+@RequestMapping("/api/families")
 public class FamilyController {
     private final FamilyService familyService;
 
@@ -13,4 +15,23 @@ public class FamilyController {
         this.familyService = familyService;
     }
 
+    @GetMapping("/")
+    public List<Family> getAllFamilies(){
+        return familyService.gelAllFamilies();
+    }
+
+    @PostMapping("/")
+    public Family createFamily(Family family){
+        return familyService.createFamily(family);
+    }
+
+    @PutMapping("/{id}")
+    public Family updateFamily(@PathVariable(value = "id") Long id, @RequestBody Family family){
+        return familyService.updateFamily(id, family);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteFamily(@PathVariable(value = "id") Long id){
+        familyService.deleteFamily(id);
+    }
 }
