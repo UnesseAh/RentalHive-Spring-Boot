d9270d61af3d44fbeebb839a1d0dacce200df7e6
FAHD <mahfoudifahd55@gmail.com>
2023-11-20 11:01:36 +0100

    create demande service

--- src/main/java/com/rentalhive/services/demand/DemandService.java
+++ src/main/java/com/rentalhive/services/demand/DemandService.java
@@ -1,8 +1,7 @@
 package com.rentalhive.services.demand;
 
 import com.rentalhive.models.dto.DemandDTO;
-import com.rentalhive.models.dto.UserDTO;
 
 public interface DemandService {
-    public UserDTO saveDemand(DemandDTO demandDTO);
+    public DemandDTO saveDemand(DemandDTO demandDTO);
 }
